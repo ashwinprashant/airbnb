0355516b3499a982261d26849bd33d8d9e87facb
ashwinprashant <ashwin.prashant@kellogg.northwestern.edu>
2022-04-15 03:33:59 +0000

    completed practice-7

--- practice-7.sql
+++ practice-7.sql
@@ -17,3 +17,8 @@
 -- | Entire villa                        | 75       | 2021-10-12                 |
 
 
+select listings.property_type, count(reviews.listing_id), reviews.date_reviewed 
+from listings inner join reviews on listings.id = reviews.listing_id 
+group by listings.property_type
+order by max(reviews.date_reviewed)
+;
